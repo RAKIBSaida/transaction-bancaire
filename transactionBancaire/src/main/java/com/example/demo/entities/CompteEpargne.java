b65c02c23d46729b1bf97f28b0ac5c8f24a00ef3
RAKIBSaida <rakibsaida@gmail.com>
2020-02-16 12:01:53 +0000

    Create CompteEpargne.java
    Co-Authored-By: rakibsaida <rakibsaida@users.noreply.github.com>

---
+++ transactionBancaire/src/main/java/com/example/demo/entities/CompteEpargne.java
@@ -0,0 +1,25 @@
+package com.example.demo.entities;
+
+import java.util.Date;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+@Entity
+@DiscriminatorValue("CE")
+public class CompteEpargne extends Compte {
+
+	private double taux;
+	public CompteEpargne() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public double getTaux() {
+		return taux;
+	}
+
+	public void setTaux(double taux) {
+		this.taux = taux;
+	}
+ 
+}
