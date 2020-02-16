24c1952925e101da90a0098d48cdf4b3676baede
RAKIBSaida <rakibsaida@gmail.com>
2020-02-16 12:02:42 +0000

    Create IClientRepository.java
    Co-Authored-By: rakibsaida <rakibsaida@users.noreply.github.com>

---
+++ transactionBancaire/src/main/java/com/example/demo/dao/IClientRepository.java
@@ -0,0 +1,15 @@
+/*
+ * package com.example.demo.dao;
+ * 
+ * import org.springframework.data.jpa.repository.JpaRepository;
+ * 
+ * import ch.qos.logback.core.net.server.Client;
+ * 
+ * public interface IClientRepository extends JpaRepository<Client, Long> {
+ * 
+ * 
+ * 
+ * 
+ * 
+ * }
+ */
